d1f07fe9d78d26b63f52089b6cee5e4b6de41559
Rupa Shankar <rupsshankar@gmail.com>
2013-10-10 15:55:54 -0400

    First draft of make-api-request function. Very simple at the moment.

--- src/wiki_wrapper/core.clj
+++ src/wiki_wrapper/core.clj
@@ -4,6 +4,15 @@
 
 (defrecord MediaWiki [url options])
 
+(def sample-query-map
+  (hash-map :format "json"
+    :action "query"
+    :titles "Main Page"
+    :prop "revisions"
+    :rvprop "content"))
+
+(def endpoint "http://en.wikipedia.org/w/api.php?")
+
 (defn get-request
   [url]
   (let [page (client/get url)]
@@ -12,28 +21,15 @@
 
 (defn make-api-request
   "form-data is a map"
-  [url form-data]
-  )
+  [form-data]
+  (client/get (str endpoint (get-arg-string form-data))))
 
 (defn get-arg-string
-  "form-data is a map"
+  "form-data is a map. Returns a string of <key>=<value> with & interposed in between.
+  To be used for the URL."
   [form-data]
   (apply str (interpose "&" 
   	(map (fn[x] (client/generate-query-string {(name (nth x 0)) (nth x 1)})) (seq form-data)))))
-
-(def sample-query-map
-  (hash-map :format "json"
-    :action "query"
-    :titles "Main Page"
-    :prop "revisions"
-    :rvprop "content"))
-
-(def db
-  (hash-map :format "json"
-    :action "query"
-    :titles "Main Page"
-    :prop "revisions"
-    :rvprop "content"))
 
 (defn -main
   "I don't do a whole lot ... yet."
